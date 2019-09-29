6a1d8ed99e38abb4855b07bcb9351c6494132ce3
流浪者 <1286041054@qq.com>
2019-09-29 14:23:52 +0800

    补充测试代码

--- Example/StatisticSdk/SPViewController.m
+++ Example/StatisticSdk/SPViewController.m
@@ -20,6 +20,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [[SPStatisticSdk sharedInstance] initSdk:@"lifeadvisor-ios" productPublicKey:@"bf54ebf2e8734af2bc1c55187b6032a4" appleId:@"1457000928" abtestId:@"1" channelId:1 afDevKey:@"o6XxR94NFNcyL6NTzsUrRG" isLogEnable:true];
 }
 
 - (void)didReceiveMemoryWarning
